4f8cb5a22bdfb00719e38c5df67ba126161c1495
noe vaz <jonovagu@gmail.com>
2020-07-14 12:58:42 -0500

    [ADD] adding SRP sample code

---
+++ 1_single_responsability.py
@@ -0,0 +1,43 @@
+print("""\
+TASK: We want a program to save new users,
+make sure you encrypt his password.
+""")
+
+print("----------- WITHOUT Single Responsability Principle -----------")
+class New_user():
+	def __init__(self, name, password):
+		# A really complex code to encrypt the password:
+		self.__password = password[::-1]
+		# end of my complex code, now I will set basic info of the user:
+		self.name = name
+
+	def get_pass(self):
+		return self.__password
+
+user1 = New_user("Noe", "MyNicePassword")
+print(f"User: {user1.name}, Password: {user1.get_pass()}" )
+
+# You will need to change the class New_user if you only want to modify the encryption algorithm
+# Also you need to chage the class to modify things about the basic setup for the user
+
+
+print("\n----------- WITH Single Responsability Principle -----------")
+class encrypt():
+	def complex_algorithm(self, password):
+		# doing a lot of modification here for my really complex encryption algorithm:
+		return password[::-1].upper()
+
+class USER_SRP(encrypt):
+	def __init__(self, name, password):
+		super().__init__()
+		self.__password = super().complex_algorithm(password)
+		self.name = name
+
+	def get_pass(self):
+		return self.__password
+
+New_user_SRP = USER_SRP("Andy", "ThisIsMyPasword")
+print(f"User: {New_user_SRP.name}, Password: {New_user_SRP.get_pass()}" )
+
+# Now you can make changes to the encryption algorithm in the class that is in charge of that
+#    so you wont touch the USER_SRP class for this changes and viceversa
